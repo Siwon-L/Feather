8a2f15e4d482f389d7fbc094a9c8580c1359ee83
Siwon-L <111dltldnjs@gmail.com>
2025-02-28 23:05:51 +0900

    fix: setImageURL를 통해 url 주입 시 다운로드를 시작하도록 수정
    - 기존 끝나지 않은 다운로드 작업이 있는 경우 취소 후 새 작업 실행

--- Sources/Feather/FTImageView.swift
+++ Sources/Feather/FTImageView.swift
@@ -1,50 +1,29 @@
 import UIKit
 
 open class FTImageView: UIImageView {
-  private let imageURL: URL
   private let imageDownloader: FTDownloader = .shared
-  
-  // 다운로드 작업을 보관하기 위한 프로퍼티 추가
   private var downloadTask: Task<Void, any Error>? = nil
   
-  public init(_ imageURL: URL) {
-    self.imageURL = imageURL
-    super.init(frame: .zero)
-  }
-  
-  @available(*, unavailable)
-  required public init?(coder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-  
-  override open func layoutSubviews() {
-    super.layoutSubviews()
-    // 기존 작업이 있으면 취소
+  deinit {
     downloadTask?.cancel()
+    downloadTask = nil
+  }
+  
+  public func setImageURL(_ url: URL) {
+    downloadTask?.cancel()
+    downloadTask = nil
+    image = nil
     
     // 새로운 다운로드 작업 시작
-    downloadTask = Task { [weak self] in
+    downloadTask = Task.detached { [weak self] in
       guard let self = self else { return }
       // 다운로드 작업이 취소되었는지 중간에 확인할 수 있음
-      let data = try await imageDownloader.download(url: imageURL)
+      let data = try await imageDownloader.download(url: url)
       if Task.isCancelled { return }
       let image = UIImage(data: data)
       await MainActor.run {
         self.image = image
       }
     }
-  }
-  
-  // 뷰가 윈도우에서 제거될 때 다운로드 작업 취소
-  override open func didMoveToWindow() {
-    super.didMoveToWindow()
-    if window == nil {
-      downloadTask?.cancel()
-    }
-  }
-  
-  deinit {
-    // 뷰가 deinit될 때도 다운로드 작업 취소
-    downloadTask?.cancel()
   }
 }
