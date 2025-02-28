ef7afbe07a5e89169a949f6d1a07088cbceb42e0
Siwon-L <111dltldnjs@gmail.com>
2025-02-28 14:38:32 +0900

    feat: FTCacheInfo

---
+++ Sources/Feather/FTCacheInfo.swift
@@ -0,0 +1,14 @@
+//
+//  FTCacheInfo.swift
+//  Feather
+//
+//  Created by 이시원 on 2/28/25.
+//
+
+import Foundation
+
+struct FTCacheInfo {
+  let imageURL: URL
+  let eTag: String?
+  let modified: String?
+}
