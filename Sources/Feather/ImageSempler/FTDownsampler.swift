082988aee716da8de6b5cf82e0e446f117628a1d
Siwon-L <111dltldnjs@gmail.com>
2025-03-14 15:33:31 +0900

    feat: downsample

--- Sources/Feather/ImageSempler/FTDownsampler.swift
+++ Sources/Feather/ImageSempler/FTDownsampler.swift
@@ -5,6 +5,16 @@
 //  Created by 이시원 on 3/14/25.
 //
 
+import Foundation
+import ImageIO
+
 struct FTDownsampler {
-  
+  func downsample(_ imageURL: URL, pixelSize: CGFloat) async -> CGImage? {
+    guard let imageSource = CGImageSourceCreateWithURL(imageURL as CFURL, nil) else { return nil }
+    let downsampleOptions: [NSString: Any] = [
+      kCGImageSourceCreateThumbnailFromImageAlways: true,
+      kCGImageSourceThumbnailMaxPixelSize: pixelSize
+    ]
+    return CGImageSourceCreateThumbnailAtIndex(imageSource, 0, downsampleOptions as CFDictionary)
+  }
 }
---
+++ Tests/FeatherTests/FTDownsamplerTests.swift
@@ -0,0 +1,32 @@
+//
+//  FTDownsamplerTests.swift
+//  Feather
+//
+//  Created by 이시원 on 3/14/25.
+//
+
+import XCTest
+@testable import Feather
+
+final class FTDownsamplerTests: XCTestCase {
+  var sut: FTDownsampler!
+  
+  override func setUp() {
+    sut = FTDownsampler()
+  }
+  
+  override func tearDown() {
+    sut = nil
+  }
+  
+  func test_downsample() {
+    // Arrange
+    let imageURL = Bundle.module.url(forResource: "dummy_image", withExtension: "png")
+    let originalData = UIImage(contentsOfFile: imageURL!.path())!.pngData()!
+    // Act
+    let cgImage = sut.downsample(imageURL!, pixelSize: 100)!
+    let downsampledData = UIImage(cgImage: cgImage).pngData()!
+    // Assert
+    XCTAssertTrue(originalData.count > downsampledData.count)
+  }
+}
